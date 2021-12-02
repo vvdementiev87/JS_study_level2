020b52c0cee169fd7dad0c68898432e96279bc9d
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2021-12-02 22:39:41 +0300

    update#2 02.12.2021
    HW#2

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,88 @@
-const goods = [
-    { id: 1, title: 'Shirt', price: 150 },
-    { id: 2, title: 'Socks', price: 50 },
-    { id: 3, title: 'Jacket', price: 350 },
-    { id: 4, title: 'Shoes', price: 250 },
-    { id: 1, title: 'Shirt', price: 150 },
-    { id: 2, title: 'Socks', price: 50 },
-    { id: 3, title: 'Jacket', price: 350 },
-    { id: 4, title: 'Shoes', price: 250 },
-    { id: 1, title: 'Shirt', price: 150 },
-    { id: 2, title: 'Socks', price: 50 },
-    { id: 3, title: 'Jacket', price: 350 },
-    { id: 4, title: 'Shoes', price: 250 },
-];
-/**
- * Для уменьшения количества аргументов передается целиком объект
- * @param {*} item 
- */
-const renderGoodsItem = (item) => {
-    return `<div class="goods-item" data-id="${item.id}">
-    <h3>${item.title}</h3>
-    <p>$${item.price}</p>
-    <button class="buy-btn">Купить</button></div>`;
-};
-/**
- * в качестве значенияпо умолчанию передается массив с одним элементом [{ id: 1, title: 'empty', price: 0 }]
- * @param {*} list  
- */
-const renderGoodsList = (list = [{ id: 1, title: 'empty', price: 0 }]) => {
-    let goodsList = list.map(item => renderGoodsItem(item));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-    // запятая возникает из-за неявного преобразования массива в строку
-    //для удаления запятой используем метод массива join и в качестве сеператора ставим пустйю строку
+class ProductList {
+    constructor(container = '.products__list') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+        this.totalSum();
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Shirt', price: 150, img: 'img1.png' },
+            { id: 2, title: 'Socks', price: 50, img: 'img2.png' },
+            { id: 3, title: 'Jacket', price: 350, img: 'img3.png' },
+            { id: 4, title: 'Shoes', price: 250, img: 'img4.png' }
+        ];
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', item.render());
+        }
+
+    }
+    totalSum() {
+        let totalSum = 0;
+        for (let item of this.goods) {
+            totalSum += item.price;
+        }
+        return totalSum
+    }
 }
 
-renderGoodsList(goods);
+class ProductItem {
+    constructor(product) {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = product.img;
+        this.render();
+    }
+    render() {
+        return `<div class="products__item" data-id="${this.id}">
+        <img class="products__img" src="images/${this.img}">
+        <h3>${this.title}</h3>
+        <p>$${this.price}</p>
+        <button class="buy__btn">Купить</button></div>`
+    }
+}
+
+class Cart {
+    constructor(container = '.cart__count') {
+        this.container = container;
+        this.addProduct();
+        this.deleteProduct();
+        this.drawCart();
+        this.totalSumCount();
+        this.cartHiddenToggle();
+    }
+    cartHiddenToggle() {
+        document.querySelector(this.container).classList.toggle('cart__hidden');
+    }
+
+    addProduct() {}
+    deleteProduct() {}
+    drawCart() {}
+    totalSumCount() {}
+}
+
+class ProductInCart {
+    constructor() {
+        this.productInCart = [];
+        this.drawNewCartRow();
+        this.updateCartRow();
+    }
+    drawNewCartRow() {}
+    updateCartRow() {}
+}
+
+
+let productList = new ProductList;
+console.log(productList.totalSum());
+
+
+let cart = new Cart;
+document.querySelector('.cart__button').addEventListener('click', function() {
+    cart.cartHiddenToggle();
+});
