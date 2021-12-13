c10f2ae3448dfe21cea68311dd52fe04fbb3b568
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2021-12-13 21:44:27 +0300

    update#3 13.12.2021

--- JS/script-vue.js
+++ JS/script-vue.js
@@ -1,37 +1,44 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 
 
-const app=new Vue({
-    el:"#app",
-    data:{
+const app = new Vue({
+    el: "#app",
+    data: {
         catalogUrl: 'catalogData.json',
         products: [],
         filtered: [],
         userSearch: '',
         show: false
     },
-    methods:{
-        filter(value){
-            const regexp=new RegExp(value,'i');
-            this.filtered=this.products.filter(product=>regexp.test(product.product_name));
+    methods: {
+        filter(value) {
+            const regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(product => regexp.test(product.product_name));
         },
-        getJson(url){
+        getJson(url) {
             return fetch(url)
-            .then(result=>result.getJson())
-            .catch(error=>{
-                console.log(error);
-            })
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
         },
-        addProduct(product){
+        addProduct(product) {
             console.log(product.id_product);
         }
     },
-    mounted(){
+    mounted() {
         this.getJson(`${API+this.catalogUrl}`)
-        .then(data=>{
-            for(let el of data){
-                this.products.push(el);
-            }
-        })
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+        this.getJson('getProducts.json')
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            })
+
     }
 })
