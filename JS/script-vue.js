7909b0a072095ce9d266c6ba6a236bfcdbcb2822
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2021-12-13 16:35:47 +0300

    update#3 13.12.2021

--- JS/script-vue.js
+++ JS/script-vue.js
@@ -1,3 +1,37 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+
+
 const app=new Vue({
     el:"#app",
+    data:{
+        catalogUrl: 'catalogData.json',
+        products: [],
+        filtered: [],
+        userSearch: '',
+        show: false
+    },
+    methods:{
+        filter(value){
+            const regexp=new RegExp(value,'i');
+            this.filtered=this.products.filter(product=>regexp.test(product.product_name));
+        },
+        getJson(url){
+            return fetch(url)
+            .then(result=>result.getJson())
+            .catch(error=>{
+                console.log(error);
+            })
+        },
+        addProduct(product){
+            console.log(product.id_product);
+        }
+    },
+    mounted(){
+        this.getJson(`${API+this.catalogUrl}`)
+        .then(data=>{
+            for(let el of data){
+                this.products.push(el);
+            }
+        })
+    }
 })
