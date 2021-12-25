34343102d46737821243dabb5ecf3e4f6e8fad0a
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2021-12-25 14:08:58 +0300

    update #3

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,38 @@
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    entry: '../js/CartComponent',
-    outpu: {
-        filename: './build.js'
-    }
+    mode: 'development',
+    entry: ['./public/js/main', './public/js/CartComponent', './public/js/ErrorComp', './public/js/FilterComp', './public/js/ProductComponent'],
+    output: {
+        filename: "./build.js"
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new VueLoaderPlugin(),
+    ]
 }
